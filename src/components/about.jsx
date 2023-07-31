6a4215973f34f1db1e178002f8c44dcc56dafd6b
delamoar <you@example.com>
2023-07-31 03:09:22 +0300

    done

---
+++ src/components/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About({image="https://via.placeholder.com/215", about}) {
+  return (
+    <div>
+        <aside>
+            <img src={image} alt="blog logo"/>
+            <p>{about}</p>
+
+        </aside>
+    </div>
+  )
+}
